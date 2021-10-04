f89884f02c9fe45e7a596149f3bd0359f535dba1
Ho Xuan Vinh <xuan-vinh.ho@etu.utc.fr>
2021-10-04 17:03:13 +0200

    test line 2

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,49 +1,93 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:tn09_app_web_demo/login_page/login_page.dart';
 import 'package:flutter/services.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   final auth = FirebaseAuth.instance;
+  String dropdownValue = 'One';
+  ElevatedButton button1 = ElevatedButton(onPressed: () {}, child: Text('12'));
   @override
   Widget build(BuildContext context) {
     // final applicationBloc = Provider.of<ApplicationBloc>(context);
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            height: 80,
-            color: Colors.green,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    margin:
-                        const EdgeInsets.only(left: 10, top: 10, bottom: 10),
-                    child: Image.asset('images/logo_homescreen.png'),
-                  ),
-                  Container(
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 80,
+              color: Colors.green,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
                       margin:
-                          const EdgeInsets.only(right: 10, top: 20, bottom: 20),
-                      child: GestureDetector(
-                        child: Row(
-                          children: [
-                            Image.asset('images/logo_user.png'),
-                            SizedBox(width: 10),
-                            Text(
-                              'User Name Here',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15,
+                          const EdgeInsets.only(left: 10, top: 10, bottom: 10),
+                      child: Image.asset('images/logo_homescreen.png'),
+                    ),
+                    Container(
+                        margin: const EdgeInsets.only(
+                            right: 10, top: 20, bottom: 20),
+                        child: GestureDetector(
+                          child: Row(
+                            children: [
+                              Image.asset('images/logo_user.png'),
+                              SizedBox(width: 10),
+                              Text(
+                                'User Name Here',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
+                        )),
+                  ]),
+            ),
+            Container(
+              color: Colors.red,
+              width: double.infinity,
+              height: 50,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 20,
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.blue,
                         ),
-                      )),
-                ]),
-          ),
-        ],
+                        child: Padding(
+                          padding: EdgeInsets.all(18),
+                          child: Text('Button 1 Longll',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                              )),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
