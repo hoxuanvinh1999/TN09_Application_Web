d100022cba937935d9b7364e0a19b07187ba9e30
Ho Xuan Vinh <hoxuanvinh1999@gmail.com>
2021-10-08 13:42:27 +0200

    create partenaire page and add to menu 2

---
+++ lib/pages/partenaire_page.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tn09_app_web_demo/header.dart';
+import 'package:tn09_app_web_demo/menu/menu.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class PartenairePage extends StatefulWidget {
+  @override
+  _PartenairePageState createState() => _PartenairePageState();
+}
+
+class _PartenairePageState extends State<PartenairePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Column(children: [
+      header(context: context),
+      menu(context: context),
+      SizedBox(height: 20),
+      Align(
+          alignment: Alignment(-0.9, 0),
+          child: Container(
+              margin: EdgeInsets.only(left: 20),
+              width: 800,
+              height: 1000,
+              color: Colors.green,
+              child: Column(children: []))),
+    ])));
+  }
+}
