1d3daad5c4ecd8221a4154e603b229096537d3c9
Ho Xuan Vinh <hoxuanvinh1999@gmail.com>
2021-10-08 14:38:20 +0200

    create contact page

--- lib/menu/showSubMenu2.dart
+++ lib/menu/showSubMenu2.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tn09_app_web_demo/pages/collecteur_page.dart';
+import 'package:tn09_app_web_demo/pages/contact_page.dart';
 import 'package:tn09_app_web_demo/pages/partenaire_page.dart';
 
 showSubMenu2({required BuildContext context}) {
@@ -32,6 +33,24 @@ showSubMenu2({required BuildContext context}) {
                           child: Padding(
                             padding: EdgeInsets.only(left: 10, top: 5),
                             child: Text('Partenaire'),
+                          )),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (context) => ContactPage()));
+                      },
+                      child: Container(
+                          margin: EdgeInsets.only(left: 0),
+                          color: Colors.red,
+                          width: 400,
+                          height: 30,
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 10, top: 5),
+                            child: Text('Contact'),
                           )),
                     ),
                     SizedBox(
---
+++ lib/pages/contact_page.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tn09_app_web_demo/header.dart';
+import 'package:tn09_app_web_demo/menu/menu.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ContactPage extends StatefulWidget {
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Column(children: [
+      header(context: context),
+      menu(context: context),
+      SizedBox(height: 20),
+    ])));
+  }
+}
