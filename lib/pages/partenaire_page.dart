9483db4e9797d5630afd58b249b488c4bf4a31c9
Ho Xuan Vinh <hoxuanvinh1999@gmail.com>
2021-10-08 13:53:20 +0200

    add line 1 and 2 for partenaire page

--- lib/pages/partenaire_page.dart
+++ lib/pages/partenaire_page.dart
@@ -27,7 +27,136 @@ class _PartenairePageState extends State<PartenairePage> {
               width: 800,
               height: 1000,
               color: Colors.green,
-              child: Column(children: []))),
+              child: Column(children: [
+                Container(
+                  color: Colors.blue,
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(width: 20),
+                          Icon(
+                            FontAwesomeIcons.building,
+                            size: 17,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Partenaire',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 500,
+                          ),
+                          Container(
+                              width: 150,
+                              decoration: BoxDecoration(
+                                  color: Colors.yellow,
+                                  borderRadius: BorderRadius.circular(10)),
+                              margin: const EdgeInsets.only(
+                                  right: 10, top: 20, bottom: 20),
+                              child: GestureDetector(
+                                onTap: () {
+                                  //Update later
+                                },
+                                child: Row(
+                                  children: [
+                                    Icon(
+                                      Icons.add,
+                                      color: Colors.white,
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      'New Parenaire',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      const Divider(
+                        thickness: 5,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                    color: Colors.red,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Icon(
+                              FontAwesomeIcons.building,
+                              size: 17,
+                            ),
+                            SizedBox(
+                              width: 30,
+                            ),
+                            Text(
+                              'Actif',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 100,
+                            ),
+                            Text(
+                              'Nom Partenaire',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 300,
+                            ),
+                            Icon(
+                              FontAwesomeIcons.user,
+                              size: 17,
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        const Divider(
+                          thickness: 5,
+                        ),
+                        SizedBox(
+                          height: 15,
+                        )
+                      ],
+                    )),
+              ]))),
     ])));
   }
 }
