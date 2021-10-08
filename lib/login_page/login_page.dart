5b728b92b60bd919701ed33e7a60914c374aa1f4
Ho Xuan Vinh <xuan-vinh.ho@etu.utc.fr>
2021-10-08 11:59:20 +0200

    block back button in Login Page

--- lib/login_page/login_page.dart
+++ lib/login_page/login_page.dart
@@ -15,174 +15,178 @@ class _LoginPageState extends State<LoginPage> {
   final auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                Color(0xFFBFAC97),
-                Color(0xFF74B424),
-                Color(0xFF94C21E),
-              ])),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 80,
-                ),
-                Image.asset('images/app_logo.png'),
-                SizedBox(
-                  height: 15,
-                ),
-                Text(
-                  'Les Detritivores',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                  Color(0xFFBFAC97),
+                  Color(0xFF74B424),
+                  Color(0xFF94C21E),
+                ])),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 80,
                   ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  height: 480,
-                  width: 400,
-                  decoration: BoxDecoration(
+                  Image.asset('images/app_logo.png'),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    'Les Detritivores',
+                    style: TextStyle(
                       color: Colors.white,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: Column(
-                    children: [
-                      SizedBox(height: 30),
-                      Text(
-                        'Log In Page',
-                        style: TextStyle(
-                            fontSize: 35, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        'Log into our DataBase',
-                        style: TextStyle(fontSize: 15, color: Colors.grey),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        width: 300,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            labelText: 'Email Address',
-                            suffixIcon: Icon(
-                              FontAwesomeIcons.envelope,
-                              size: 17,
-                            ),
-                          ),
-                          onChanged: (value) {
-                            setState(() {
-                              _email = value.trim();
-                            });
-                          },
-                        ),
-                      ),
-                      Container(
-                        width: 300,
-                        child: TextField(
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            labelText: 'Password',
-                            suffixIcon: Icon(
-                              FontAwesomeIcons.eyeSlash,
-                              size: 17,
-                            ),
-                          ),
-                          onChanged: (value) {
-                            setState(() {
-                              _password = value.trim();
-                            });
-                          },
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(20, 20, 40, 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text(
-                              'Forget Password',
-                              style: TextStyle(color: Colors.orangeAccent[700]),
-                            )
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      GestureDetector(
-                        onTap: () => _signin(_email, _password),
-                        child: Container(
-                          alignment: Alignment.center,
-                          width: 300,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              gradient: LinearGradient(
-                                  begin: Alignment.centerLeft,
-                                  end: Alignment.bottomRight,
-                                  colors: [
-                                    Color(0xFFBFAC97),
-                                    Color(0xFF74B424),
-                                    Color(0xFF94C21E),
-                                  ])),
-                          child: Padding(
-                            padding: EdgeInsets.all(12.0),
-                            child: Text('Sign In',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      GestureDetector(
-                        onTap: () {
-                          showSignUpDialog(context: context);
-                        },
-                        child: Container(
-                          alignment: Alignment.center,
-                          width: 300,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50),
-                              gradient: LinearGradient(
-                                  begin: Alignment.centerLeft,
-                                  end: Alignment.bottomRight,
-                                  colors: [
-                                    Color(0xFFBFAC97),
-                                    Color(0xFF74B424),
-                                    Color(0xFF94C21E),
-                                  ])),
-                          child: Padding(
-                            padding: EdgeInsets.all(12.0),
-                            child: Text('Sign Up',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                )),
-                          ),
-                        ),
-                      ),
-                    ],
+                      fontSize: 30,
+                    ),
                   ),
-                ),
-              ],
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    height: 480,
+                    width: 400,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Column(
+                      children: [
+                        SizedBox(height: 30),
+                        Text(
+                          'Log In Page',
+                          style: TextStyle(
+                              fontSize: 35, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Log into our DataBase',
+                          style: TextStyle(fontSize: 15, color: Colors.grey),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          width: 300,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              labelText: 'Email Address',
+                              suffixIcon: Icon(
+                                FontAwesomeIcons.envelope,
+                                size: 17,
+                              ),
+                            ),
+                            onChanged: (value) {
+                              setState(() {
+                                _email = value.trim();
+                              });
+                            },
+                          ),
+                        ),
+                        Container(
+                          width: 300,
+                          child: TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              labelText: 'Password',
+                              suffixIcon: Icon(
+                                FontAwesomeIcons.eyeSlash,
+                                size: 17,
+                              ),
+                            ),
+                            onChanged: (value) {
+                              setState(() {
+                                _password = value.trim();
+                              });
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(20, 20, 40, 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                'Forget Password',
+                                style:
+                                    TextStyle(color: Colors.orangeAccent[700]),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        GestureDetector(
+                          onTap: () => _signin(_email, _password),
+                          child: Container(
+                            alignment: Alignment.center,
+                            width: 300,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                gradient: LinearGradient(
+                                    begin: Alignment.centerLeft,
+                                    end: Alignment.bottomRight,
+                                    colors: [
+                                      Color(0xFFBFAC97),
+                                      Color(0xFF74B424),
+                                      Color(0xFF94C21E),
+                                    ])),
+                            child: Padding(
+                              padding: EdgeInsets.all(12.0),
+                              child: Text('Sign In',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  )),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        GestureDetector(
+                          onTap: () {
+                            showSignUpDialog(context: context);
+                          },
+                          child: Container(
+                            alignment: Alignment.center,
+                            width: 300,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                gradient: LinearGradient(
+                                    begin: Alignment.centerLeft,
+                                    end: Alignment.bottomRight,
+                                    colors: [
+                                      Color(0xFFBFAC97),
+                                      Color(0xFF74B424),
+                                      Color(0xFF94C21E),
+                                    ])),
+                            child: Padding(
+                              padding: EdgeInsets.all(12.0),
+                              child: Text('Sign Up',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  )),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
