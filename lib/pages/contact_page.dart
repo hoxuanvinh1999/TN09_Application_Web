9753189ab06b0ad3ea6314054981577bfae626fa
Ho Xuan Vinh <hoxuanvinh1999@gmail.com>
2021-10-08 14:53:07 +0200

    line 1 and 2 for contact page

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -20,6 +20,151 @@ class _ContactPageState extends State<ContactPage> {
       header(context: context),
       menu(context: context),
       SizedBox(height: 20),
+      Align(
+          alignment: Alignment(-0.9, 0),
+          child: Container(
+              margin: EdgeInsets.only(left: 20),
+              width: 1200,
+              height: 1000,
+              color: Colors.green,
+              child: Column(children: [
+                Container(
+                  color: Colors.blue,
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(width: 20),
+                          Icon(
+                            FontAwesomeIcons.user,
+                            size: 17,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Contact',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 900,
+                          ),
+                          Container(
+                              width: 150,
+                              decoration: BoxDecoration(
+                                  color: Colors.yellow,
+                                  borderRadius: BorderRadius.circular(10)),
+                              margin: const EdgeInsets.only(
+                                  right: 10, top: 20, bottom: 20),
+                              child: GestureDetector(
+                                onTap: () {
+                                  //Update later
+                                },
+                                child: Row(
+                                  children: [
+                                    Icon(
+                                      Icons.add,
+                                      color: Colors.white,
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      'New Contact',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      const Divider(
+                        thickness: 5,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                    color: Colors.red,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Text(
+                              'Prenom et nom',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 150,
+                            ),
+                            Text(
+                              'Email',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 250,
+                            ),
+                            Text(
+                              'Telephone(s)',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 300,
+                            ),
+                            Text(
+                              'Partenaire(s)',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        const Divider(
+                          thickness: 5,
+                        ),
+                        SizedBox(
+                          height: 15,
+                        )
+                      ],
+                    )),
+              ]))),
     ])));
   }
 }
