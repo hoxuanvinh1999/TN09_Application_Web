b403553ad72d2ac1aa70c6df8ac9ec5f24176ee4
Ho Xuan Vinh <hoxuanvinh1999@gmail.com>
2021-10-15 10:38:24 +0200

    add type contenant page to menu

--- lib/menu/showSubMenu1.dart
+++ lib/menu/showSubMenu1.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tn09_app_web_demo/pages/collecteur_page.dart';
 import 'package:tn09_app_web_demo/pages/contenant_page.dart';
+import 'package:tn09_app_web_demo/pages/type_contenant_page.dart';
 import 'package:tn09_app_web_demo/pages/vehicule_page.dart';
 
 showSubMenu1({required BuildContext context}) {
@@ -69,6 +70,24 @@ showSubMenu1({required BuildContext context}) {
                           child: Padding(
                             padding: EdgeInsets.only(left: 10, top: 5),
                             child: Text('Contenant'),
+                          )),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (context) => TypeContenantPage()));
+                      },
+                      child: Container(
+                          margin: EdgeInsets.only(left: 20),
+                          color: Colors.red,
+                          width: 380,
+                          height: 30,
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 10, top: 5),
+                            child: Text('Type Contenant'),
                           )),
                     ),
                   ],
