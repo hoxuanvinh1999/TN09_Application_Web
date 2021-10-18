7ee61dbd606938ec60044b45e9b4de57fef1c3f7
Ho Xuan Vinh <hoxuanvinh1999@gmail.com>
2021-10-18 10:46:22 +0200

    create planning weekly page

---
+++ lib/pages/planning_weekly_page.dart
@@ -0,0 +1,98 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tn09_app_web_demo/home_screen.dart';
+import 'package:tn09_app_web_demo/pages/collecteur_page.dart';
+import 'package:tn09_app_web_demo/header.dart';
+import 'dart:async';
+import 'package:tn09_app_web_demo/login_page/login_page.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:tn09_app_web_demo/menu/menu.dart';
+import 'package:tn09_app_web_demo/menu/showSubMenu1.dart';
+
+class PlanningWeeklyPage extends StatefulWidget {
+  @override
+  _PlanningWeeklyPageState createState() => _PlanningWeeklyPageState();
+}
+
+class _PlanningWeeklyPageState extends State<PlanningWeeklyPage> {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            header(context: context),
+            menu(context: context),
+            Container(
+                color: Colors.yellow,
+                width: double.infinity,
+                height: 40,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 40,
+                    ),
+                    Icon(
+                      FontAwesomeIcons.home,
+                      size: 12,
+                    ),
+                    SizedBox(width: 5),
+                    RichText(
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: 'Home',
+                              style: TextStyle(
+                                  color: Colors.red,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.of(context).pushReplacement(
+                                      MaterialPageRoute(
+                                          builder: (context) => HomeScreen()));
+                                }),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Icon(
+                      FontAwesomeIcons.chevronCircleRight,
+                      size: 12,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Planning Weekly',
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )),
+            SizedBox(height: 20),
+            Container(
+              width: 1200,
+              height: 1000,
+              color: Colors.green,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
