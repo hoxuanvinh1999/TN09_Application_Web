37b415ac2f389a5e622c03ae76517aeaa7363fd2
Ho Xuan Vinh <hoxuanvinh1999@gmail.com>
2021-10-18 11:46:05 +0200

    create function calculer week of year

---
+++ lib/pages/math_function/week_of_year.dart
@@ -0,0 +1,18 @@
+import 'package:intl/intl.dart';
+
+int numOfWeeks(int year) {
+  DateTime dec28 = DateTime(year, 12, 28);
+  int dayOfDec28 = int.parse(DateFormat("D").format(dec28));
+  return ((dayOfDec28 - dec28.weekday + 10) / 7).floor();
+}
+
+int weekNumber(DateTime date) {
+  int dayOfYear = int.parse(DateFormat("D").format(date));
+  int woy = ((dayOfYear - date.weekday + 10) / 7).floor();
+  if (woy < 1) {
+    woy = numOfWeeks(date.year - 1);
+  } else if (woy > numOfWeeks(date.year)) {
+    woy = 1;
+  }
+  return woy;
+}
