6e78fe6b8413caed59708bb77463f47674bdd33b
Ho Xuan Vinh <hoxuanvinh1999@gmail.com>
2021-10-19 16:05:38 +0200

    add create tournee to menu3

--- lib/menu/showSubMenu3.dart
+++ lib/menu/showSubMenu3.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tn09_app_web_demo/pages/collecteur_page.dart';
 import 'package:tn09_app_web_demo/pages/contact_page.dart';
+import 'package:tn09_app_web_demo/pages/create_tournee_page.dart';
 import 'package:tn09_app_web_demo/pages/partenaire_page.dart';
 import 'package:tn09_app_web_demo/pages/planning_daily_page.dart';
 import 'package:tn09_app_web_demo/pages/planning_weekly_page.dart';
@@ -61,6 +62,24 @@ showSubMenu3({required BuildContext context}) {
                           child: Padding(
                             padding: EdgeInsets.only(left: 10, top: 5),
                             child: Text('Planning Daily'),
+                          )),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (context) => CreateTourneePage()));
+                      },
+                      child: Container(
+                          margin: EdgeInsets.only(left: 0),
+                          color: Colors.red,
+                          width: 400,
+                          height: 30,
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 10, top: 5),
+                            child: Text('Create Tournee'),
                           )),
                     ),
                   ],
