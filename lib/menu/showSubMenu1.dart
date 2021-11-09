a91ebb5cbc39df2cfe498a402b83a34af315001e
Ho Xuan Vinh <hoxuanvinh1999@gmail.com>
2021-11-09 12:28:20 +0100

    add Matiere Page to menu

--- lib/menu/showSubMenu1.dart
+++ lib/menu/showSubMenu1.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tn09_app_web_demo/pages/collecteur_page.dart';
 import 'package:tn09_app_web_demo/pages/contenant_page.dart';
+import 'package:tn09_app_web_demo/pages/matieres_page.dart';
 import 'package:tn09_app_web_demo/pages/type_contenant_page.dart';
 import 'package:tn09_app_web_demo/pages/vehicule_page.dart';
 
@@ -80,6 +81,21 @@ showSubMenu1({required BuildContext context}) {
                           child: Padding(
                             padding: EdgeInsets.only(left: 10, top: 5),
                             child: Text('Type Contenant'),
+                          )),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (context) => MatieresPage()));
+                      },
+                      child: Container(
+                          margin: EdgeInsets.only(top: 10, bottom: 10),
+                          color: Colors.red,
+                          width: 400,
+                          height: 30,
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 10, top: 5),
+                            child: Text('Matieres'),
                           )),
                     ),
                   ],
