41f60f1d602676e2a87c6034ae806510219420ad
hoxuanvinh1999 <hoxuanvinh1999@gmail.com>
2021-11-15 13:29:47 +0100

    edit submenu1

--- lib/menu/showSubMenu1.dart
+++ lib/menu/showSubMenu1.dart
@@ -1,10 +1,11 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tn09_app_web_demo/pages/collecteur_page.dart';
 import 'package:tn09_app_web_demo/pages/contenant_page.dart';
 import 'package:tn09_app_web_demo/pages/matieres_page.dart';
 import 'package:tn09_app_web_demo/pages/type_contenant_page.dart';
 import 'package:tn09_app_web_demo/pages/vehicule_page.dart';
+import 'package:tn09_app_web_demo/decoration/graphique.dart' as graphique;
 
 showSubMenu1({required BuildContext context}) {
   return showDialog(
@@ -12,12 +13,22 @@ showSubMenu1({required BuildContext context}) {
       context: context,
       builder: (BuildContext context) {
         return Dialog(
-          alignment: Alignment(-1.045, -0.04),
+          alignment: const Alignment(-1.045, -0.04),
           child: Container(
               height: 400,
               width: 200,
               decoration: BoxDecoration(
-                color: Colors.yellow,
+                color: Color(graphique.color['main_color_1']),
+                border: Border(
+                    right: BorderSide(
+                        width: 1.0,
+                        color: Color(graphique.color['defaut_black'])),
+                    left: BorderSide(
+                        width: 1.0,
+                        color: Color(graphique.color['defaut_black'])),
+                    bottom: BorderSide(
+                        width: 1.0,
+                        color: Color(graphique.color['defaut_black']))),
               ),
               child: SingleChildScrollView(
                 child: Column(
@@ -28,14 +39,41 @@ showSubMenu1({required BuildContext context}) {
                             builder: (context) => CollecteurPage()));
                       },
                       child: Container(
-                          margin: EdgeInsets.only(top: 10, bottom: 10),
-                          color: Colors.red,
-                          width: 400,
-                          height: 30,
-                          child: Padding(
-                            padding: EdgeInsets.only(left: 10, top: 5),
-                            child: Text('Collecteur'),
-                          )),
+                        margin: const EdgeInsets.only(top: 8),
+                        decoration: BoxDecoration(
+                          color: Color(graphique.color['main_color_1']),
+                        ),
+                        width: 400,
+                        height: 30,
+                        child: Padding(
+                            padding: const EdgeInsets.only(left: 10, top: 5),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  FontAwesomeIcons.users,
+                                  size: 15,
+                                  color: Color(graphique.color['main_color_2']),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  graphique.languagefr['button_1']
+                                      ['function_1_title'],
+                                  style: TextStyle(
+                                    color:
+                                        Color(graphique.color['main_color_2']),
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            )),
+                      ),
+                    ),
+                    Divider(
+                      thickness: 2,
+                      color: Color(graphique.color['secondary_color_2']),
                     ),
                     GestureDetector(
                       onTap: () {
@@ -43,14 +81,38 @@ showSubMenu1({required BuildContext context}) {
                             builder: (context) => VehiculePage()));
                       },
                       child: Container(
-                          margin: EdgeInsets.only(top: 10, bottom: 10),
-                          color: Colors.red,
+                          color: Color(graphique.color['main_color_1']),
                           width: 400,
                           height: 30,
                           child: Padding(
-                            padding: EdgeInsets.only(left: 10, top: 5),
-                            child: Text('Vehicule'),
-                          )),
+                              padding: const EdgeInsets.only(left: 10, top: 5),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    FontAwesomeIcons.truck,
+                                    size: 15,
+                                    color:
+                                        Color(graphique.color['main_color_2']),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(
+                                    graphique.languagefr['button_1']
+                                        ['function_2_title'],
+                                    style: TextStyle(
+                                      color: Color(
+                                          graphique.color['main_color_2']),
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ))),
+                    ),
+                    Divider(
+                      thickness: 2,
+                      color: Color(graphique.color['secondary_color_2']),
                     ),
                     GestureDetector(
                       onTap: () {
@@ -58,14 +120,39 @@ showSubMenu1({required BuildContext context}) {
                             builder: (context) => ContenantPage()));
                       },
                       child: Container(
-                          margin: EdgeInsets.only(top: 10, bottom: 10),
-                          color: Colors.red,
+                          color: Color(graphique.color['main_color_1']),
                           width: 400,
                           height: 30,
                           child: Padding(
-                            padding: EdgeInsets.only(left: 10, top: 5),
-                            child: Text('Contenant'),
-                          )),
+                              padding: const EdgeInsets.only(left: 10, top: 5),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    FontAwesomeIcons.boxOpen,
+                                    size: 15,
+                                    color:
+                                        Color(graphique.color['main_color_2']),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(
+                                    graphique.languagefr['button_1']
+                                        ['function_3_title'],
+                                    style: TextStyle(
+                                      color: Color(
+                                          graphique.color['main_color_2']),
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ))),
+                    ),
+                    Divider(
+                      thickness: 2,
+                      indent: 30,
+                      color: Color(graphique.color['secondary_color_2']),
                     ),
                     GestureDetector(
                       onTap: () {
@@ -73,15 +160,39 @@ showSubMenu1({required BuildContext context}) {
                             builder: (context) => TypeContenantPage()));
                       },
                       child: Container(
-                          margin:
-                              EdgeInsets.only(left: 20, top: 10, bottom: 10),
-                          color: Colors.red,
+                          margin: const EdgeInsets.only(left: 20),
+                          color: Color(graphique.color['main_color_1']),
                           width: 380,
                           height: 30,
                           child: Padding(
-                            padding: EdgeInsets.only(left: 10, top: 5),
-                            child: Text('Type Contenant'),
-                          )),
+                              padding: const EdgeInsets.only(left: 10, top: 5),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    FontAwesomeIcons.cubes,
+                                    size: 15,
+                                    color:
+                                        Color(graphique.color['main_color_2']),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(
+                                    graphique.languagefr['button_1']
+                                        ['sub_function_3_title'],
+                                    style: TextStyle(
+                                      color: Color(
+                                          graphique.color['main_color_2']),
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ))),
+                    ),
+                    Divider(
+                      thickness: 2,
+                      color: Color(graphique.color['secondary_color_2']),
                     ),
                     GestureDetector(
                       onTap: () {
@@ -89,14 +200,38 @@ showSubMenu1({required BuildContext context}) {
                             builder: (context) => MatieresPage()));
                       },
                       child: Container(
-                          margin: EdgeInsets.only(top: 10, bottom: 10),
-                          color: Colors.red,
+                          color: Color(graphique.color['main_color_1']),
                           width: 400,
                           height: 30,
                           child: Padding(
-                            padding: EdgeInsets.only(left: 10, top: 5),
-                            child: Text('Matieres'),
-                          )),
+                              padding: const EdgeInsets.only(left: 10, top: 5),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    FontAwesomeIcons.tags,
+                                    size: 15,
+                                    color:
+                                        Color(graphique.color['main_color_2']),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(
+                                    graphique.languagefr['button_1']
+                                        ['function_4_title'],
+                                    style: TextStyle(
+                                      color: Color(
+                                          graphique.color['main_color_2']),
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ))),
+                    ),
+                    Divider(
+                      thickness: 2,
+                      color: Color(graphique.color['secondary_color_2']),
                     ),
                   ],
                 ),
