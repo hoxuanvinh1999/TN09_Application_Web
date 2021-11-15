4c68a4468e2a25355a2cd82d7fdcb60de3f32337
hoxuanvinh1999 <hoxuanvinh1999@gmail.com>
2021-11-15 10:54:17 +0100

    add graphique file

---
+++ lib/decoration/graphique.dart
@@ -0,0 +1,14 @@
+const Map<String, dynamic> color = {
+  'defaut_white': 0xfff4eeee,
+  'defaut_black': 0xff060606,
+  'grey': 0xff484f41,
+  'main_color_1': 0xFF263b29,
+  'main_color_2': 0xFFe94e1a,
+  'secondary_color_1': 0xFF9eb356,
+  'secondary_color_2': 0xFFefb661,
+  'secondary_color_2_hover': 0xFFeeb560,
+};
+
+const Map<String, dynamic> languagefr = {
+  'button_1_title': 'Gestion',
+};
