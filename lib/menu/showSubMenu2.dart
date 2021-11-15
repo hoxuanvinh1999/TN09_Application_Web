bb8d8a305eac8d68f500dca191bde2e51f7da5a3
hoxuanvinh1999 <hoxuanvinh1999@gmail.com>
2021-11-15 13:29:58 +0100

    edit submenu2

--- lib/menu/showSubMenu2.dart
+++ lib/menu/showSubMenu2.dart
@@ -1,8 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:tn09_app_web_demo/pages/collecteur_page.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tn09_app_web_demo/pages/contact_page.dart';
 import 'package:tn09_app_web_demo/pages/partenaire_page.dart';
+import 'package:tn09_app_web_demo/decoration/graphique.dart' as graphique;
 
 showSubMenu2({required BuildContext context}) {
   return showDialog(
@@ -10,12 +10,22 @@ showSubMenu2({required BuildContext context}) {
       context: context,
       builder: (BuildContext context) {
         return Dialog(
-          alignment: Alignment(-0.6, -0.04),
+          alignment: const Alignment(-0.58, -0.04),
           child: Container(
               height: 400,
               width: 200,
               decoration: BoxDecoration(
-                color: Colors.blue,
+                color: Color(graphique.color['main_color_1']),
+                border: Border(
+                    right: BorderSide(
+                        width: 1.0,
+                        color: Color(graphique.color['defaut_black'])),
+                    left: BorderSide(
+                        width: 1.0,
+                        color: Color(graphique.color['defaut_black'])),
+                    bottom: BorderSide(
+                        width: 1.0,
+                        color: Color(graphique.color['defaut_black']))),
               ),
               child: SingleChildScrollView(
                 child: Column(
@@ -26,14 +36,41 @@ showSubMenu2({required BuildContext context}) {
                             builder: (context) => PartenairePage()));
                       },
                       child: Container(
-                          margin: EdgeInsets.only(top: 10, bottom: 10),
-                          color: Colors.red,
+                          margin: const EdgeInsets.only(top: 8),
+                          decoration: BoxDecoration(
+                            color: Color(graphique.color['main_color_1']),
+                          ),
                           width: 400,
                           height: 30,
                           child: Padding(
-                            padding: EdgeInsets.only(left: 10, top: 5),
-                            child: Text('Partenaire'),
-                          )),
+                              padding: const EdgeInsets.only(left: 10, top: 5),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    FontAwesomeIcons.flag,
+                                    size: 15,
+                                    color:
+                                        Color(graphique.color['main_color_2']),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(
+                                    graphique.languagefr['button_2']
+                                        ['function_1_title'],
+                                    style: TextStyle(
+                                      color: Color(
+                                          graphique.color['main_color_2']),
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ))),
+                    ),
+                    Divider(
+                      thickness: 2,
+                      color: Color(graphique.color['secondary_color_2']),
                     ),
                     GestureDetector(
                       onTap: () {
@@ -41,17 +78,38 @@ showSubMenu2({required BuildContext context}) {
                             builder: (context) => ContactPage()));
                       },
                       child: Container(
-                          margin: EdgeInsets.only(top: 10, bottom: 10),
-                          color: Colors.red,
+                          color: Color(graphique.color['main_color_1']),
                           width: 400,
                           height: 30,
                           child: Padding(
-                            padding: EdgeInsets.only(left: 10, top: 5),
-                            child: Text('Contact'),
-                          )),
+                              padding: const EdgeInsets.only(left: 10, top: 5),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    FontAwesomeIcons.user,
+                                    size: 15,
+                                    color:
+                                        Color(graphique.color['main_color_2']),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text(
+                                    graphique.languagefr['button_2']
+                                        ['function_2_title'],
+                                    style: TextStyle(
+                                      color: Color(
+                                          graphique.color['main_color_2']),
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ))),
                     ),
-                    SizedBox(
-                      height: 10,
+                    Divider(
+                      thickness: 2,
+                      color: Color(graphique.color['secondary_color_2']),
                     ),
                   ],
                 ),
