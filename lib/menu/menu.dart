82fb2b3a604bd6931cc5d00cdaa29b6a0a429b1f
hoxuanvinh1999 <hoxuanvinh1999@gmail.com>
2021-11-15 10:55:36 +0100

    edit menu

--- lib/menu/menu.dart
+++ lib/menu/menu.dart
@@ -1,79 +1,103 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:tn09_app_web_demo/menu/showSubMenu3.dart';
-import 'package:tn09_app_web_demo/pages/collecteur_page.dart';
-import 'package:tn09_app_web_demo/menu/showSubMenu1.dart';
 import 'package:tn09_app_web_demo/menu/showSubMenu2.dart';
+import 'package:tn09_app_web_demo/menu/showSubMenu3.dart';
+import 'package:tn09_app_web_demo/menu/showSubMenu1.dart';
+import 'package:tn09_app_web_demo/decoration/graphique.dart' as graphique;
 
 Widget menu({required BuildContext context}) {
   return Container(
-    color: Colors.red,
+    decoration: BoxDecoration(
+      color: Color(graphique.color['secondary_color_1']),
+      border: Border(
+        bottom: BorderSide(
+            width: 1.0, color: Color(graphique.color['defaut_black'])),
+      ),
+    ),
     width: double.infinity,
     height: 50,
     child: SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
         children: [
-          SizedBox(
-            width: 20,
-          ),
-          GestureDetector(
+          InkWell(
             onTap: () {
               showSubMenu1(context: context);
             },
             child: Container(
+              margin: const EdgeInsets.only(left: 20, right: 10),
               alignment: Alignment.center,
               width: 200,
-              decoration: BoxDecoration(color: Colors.blue),
+              decoration: BoxDecoration(
+                color: Color(graphique.color['main_color_1']),
+              ),
               child: Padding(
-                padding: EdgeInsets.all(12.0),
-                child: Text('Button1',
+                padding: const EdgeInsets.all(12.0),
+                child: Text(graphique.languagefr['button_1_title'],
                     style: TextStyle(
-                      color: Colors.white,
+                      color: Color(graphique.color['main_color_2']),
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
                     )),
               ),
             ),
           ),
-          SizedBox(
-            width: 10,
-          ),
-          GestureDetector(
+          // GestureDetector(
+          //   onTap: () {
+          //     showSubMenu1(context: context);
+          //   },
+          //   child: Container(
+          //     alignment: Alignment.center,
+          //     width: 200,
+          //     decoration: BoxDecoration(color: Colors.blue),
+          //     child: Padding(
+          //       padding: EdgeInsets.all(12.0),
+          //       child: Text('Button1',
+          //           style: TextStyle(
+          //             color: Colors.white,
+          //             fontSize: 20,
+          //             fontWeight: FontWeight.bold,
+          //           )),
+          //     ),
+          //   ),
+          // ),
+          InkWell(
             onTap: () {
               showSubMenu2(context: context);
             },
             child: Container(
+              margin: const EdgeInsets.only(left: 10, right: 10),
               alignment: Alignment.center,
               width: 200,
-              decoration: BoxDecoration(color: Colors.blue),
+              decoration: BoxDecoration(
+                color: Color(graphique.color['main_color_1']),
+              ),
               child: Padding(
-                padding: EdgeInsets.all(12.0),
+                padding: const EdgeInsets.all(12.0),
                 child: Text('Button2',
                     style: TextStyle(
-                      color: Colors.white,
+                      color: Color(graphique.color['main_color_2']),
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
                     )),
               ),
             ),
           ),
-          SizedBox(
-            width: 10,
-          ),
-          GestureDetector(
+          InkWell(
             onTap: () {
               showSubMenu3(context: context);
             },
             child: Container(
+              margin: const EdgeInsets.only(left: 10, right: 10),
               alignment: Alignment.center,
               width: 200,
-              decoration: BoxDecoration(color: Colors.blue),
+              decoration: BoxDecoration(
+                color: Color(graphique.color['main_color_1']),
+              ),
               child: Padding(
-                padding: EdgeInsets.all(12.0),
+                padding: const EdgeInsets.all(12.0),
                 child: Text('Button3',
                     style: TextStyle(
-                      color: Colors.white,
+                      color: Color(graphique.color['main_color_2']),
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
                     )),
