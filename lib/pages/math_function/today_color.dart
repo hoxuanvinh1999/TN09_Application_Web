69294ae1d4c448fb9686e79ffe689f4b4a295fff
hoxuanvinh1999 <hoxuanvinh1999@gmail.com>
2021-11-19 11:45:19 +0100

    edit today color function

--- lib/pages/math_function/today_color.dart
+++ lib/pages/math_function/today_color.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:tn09_app_web_demo/decoration/graphique.dart' as graphique;
 
 Color today_color({required DateTime check_day}) {
-  Color result = Colors.red;
+  Color result = Color(graphique.color['default_red']);
   if (check_day.day == DateTime.now().day &&
       check_day.month == DateTime.now().month &&
       check_day.year == DateTime.now().year) {
-    result = Color(0xFF296F03);
+    result = Color(graphique.color['default_green']);
   }
   return result;
 }
